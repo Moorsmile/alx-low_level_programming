03c6897837e08cec4a375f8c7f0ddd58a5105dba
Moorsmile <moyoogunleye08@gmail.com>
2022-09-13 07:10:03 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point 
+ * Description: a function that prints alphabet x10 times 
+ * in lowercase followed by a newline
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+
+		_putchar('\n');
+
+		i++;
+	}
+}
