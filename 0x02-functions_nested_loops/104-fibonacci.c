7fdedb3643fce883afdde34a79a7c4f48be35098
Moorsmile <moyoogunleye08@gmail.com>
2022-09-13 15:56:56 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+/**
+ * numLength - returns the length of string
+ * @num: operand number
+ * Return: number of digits
+ */
+
+int numLength(int num)
+{
+	int length = 0;
+
+	if (!num)
+	{
+		return (1);
+	}
+
+	while (num)
+	{
+		num = num / 10;
+		length += 1;
+	}
+
+	return (length);
+}
+/**
+ * *main - prints the first 98 fibonacci sequences
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f10 = 0, tmpo = 0;
+	short int i = 1, initial0s;
+
+	while (i <= 98)
+	{
+		if (f10 > 0)
+			printf("%lu", f10);
+		initial0s = numLength(mx) - 1 - numLength(f1);
+		while (f10 > 0 && initial0s > 0)
+		{
+			printf("%lu", f1);
+
+			tmp = f
