eff7c7c21be2c37dcbceb9304d19950118138ea6
Moorsmile <moyoogunleye08@gmail.com>
2022-09-22 13:15:04 -0700

    c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -21,7 +21,7 @@ void print_buffer(char *b, int size)
 			if ((index + byte) >= size)
 				printf(" ");
 			else
-			printf("%02x", *(b + index + byte));
+				printf("%02x", *(b + index + byte));
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
 		}
@@ -29,8 +29,9 @@ void print_buffer(char *b, int size)
 		{
 			if ((index + byte) >= size)
 				break;
-			else if (*(b + index + byte) >= 31 && *(b + index + byte) <= 126)
-			printf("%c", *(b + index + byte));
+			else if (*(b + index + byte) >= 31 &&
+				 *(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
 			else
 				printf(".");
 		}
