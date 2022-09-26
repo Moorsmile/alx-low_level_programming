ac18f28132b24766654d06d95605bb6953ff144e
Moorsmile <moyoogunleye08@gmail.com>
2022-09-26 09:21:15 -0700

    0x07

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,16 +4,15 @@
  * _strchr - locates a character in a string
  * @s: string to be searched
  * @c: character
- * Return: NULL
+ * Return: pointer to spot in s with c or null
  */
 
 char *_strchr(char *s, char c)
 {
-	do{
-		if (*s == c)
-		{
-			return (s);
-		}
-	} while (*s++);
-	return (NULL);
-}
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
