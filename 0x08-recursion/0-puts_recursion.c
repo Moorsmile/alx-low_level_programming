1f0a1f1120e5ec24f00f205bc367236a49fa9032
Moorsmile <moyoogunleye08@gmail.com>
2022-09-28 06:38:10 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,21 +2,19 @@
 
 /**
  * _puts_recursion - prints a string
- * @s: pointer
+ * @s: string
  * Return: void
  */
 
 void _puts_recursion(char *s);
 {
 	if (*s == '\0')
-}
-
-{
-_putchar('\n');
-return;
-}
-
-{
-_putchar(*s);
-_puts_recursion(s + 1);
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
 }
