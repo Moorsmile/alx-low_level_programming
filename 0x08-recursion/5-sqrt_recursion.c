de4714fce53c62a57898e5ffb7f0e192bf15a8c8
Moorsmile <moyoogunleye08@gmail.com>
2022-09-28 07:59:52 -0700

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,6 @@
 
 /**
  * _sqrt_recursion - natural square root of a number
- * _sqrt - returns the square root of a number
  * @n: number
  * Return: square root of the number
  */
@@ -11,7 +10,7 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1 || n == 0)
 		return (n);
-	return (_sqrt(0, n));
+	return (_sqrt_recursion(0, n));
 }
 
 /**
